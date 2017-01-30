b94b8530a5618e3927c4f7306dfe127735b3d472
Vlad Gorbich <vladgorbich@gmail.com>
2017-01-30 10:40:51 -0600

    Add Card component

---
+++ app/card/card.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'app-card',
+    templateUrl: 'card.component.html',
+    styleUrls: [ 'card.component.css' ] 
+})
+export class CardComponent { }
